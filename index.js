d59135d9b3854d60923b45e828011f6b0181085e
Sheena Sang <sheenasany@gmail.com>
2022-06-29 12:53:45 -0400

    commit tree 4a4a11f37f41fdf8258194e346f9918dd7fc27c5

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', clickAlert);
+
+    function clickAlert() {
+  alert('Hee hee, that tickles!');
 }
+
+}
+addingEventListener();
